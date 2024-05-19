967816e3ccdd76e40a7387dc6a65df0438392f50
Manahil <161109728+manahilhafiza@users.noreply.github.com>
2024-05-19 16:06:44 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-var famous_person = ("Albert Einstein");
-var message = "".concat(famous_person, " once said, \u201CA person who never made a mistake never tried anything new.");
-console.log(message);
+//Question 3: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
+var personName = " \t\n taha \t\n";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
--- app.ts
+++ app.ts
@@ -1,4 +1,10 @@
+//Question 3: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
+let personName: string =   ` \t\n taha \t\n`;
 
-let famous_person: string = ("Albert Einstein");
-let message: string = `${famous_person} once said, “A person who never made a mistake never tried anything new.`
-console.log(message);
+console.log(personName);
+
+let stripped: string= personName.trim();
+
+console.log(stripped);
+
+

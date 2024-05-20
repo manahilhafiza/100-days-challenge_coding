fe177843cbe42542b7294e503452720dcdab0f7b
Manahil <161109728+manahilhafiza@users.noreply.github.com>
2024-05-20 22:00:00 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
-console.log(5 + 3);
-console.log(11 - 3);
-console.log(4 * 2);
-console.log(16 / 2);
+var favoriteNumber = 3;
+console.log("My favorite number is  ".concat(favoriteNumber, " "));
--- app.ts
+++ app.ts
@@ -1,4 +1,2 @@
-console.log(5 + 3 );
-console.log(11 - 3);
-console.log(4 * 2);
-console.log(16 / 2 );
+let favoriteNumber: number = 3;
+console.log(`My favorite number is  ${favoriteNumber} `);

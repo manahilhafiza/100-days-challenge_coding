7e888c19b94a750c2872ccd09188494bc41bd333
Manahil <161109728+manahilhafiza@users.noreply.github.com>
2024-05-21 21:53:43 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-var guestList = ['minal', 'zaid', 'saad'];
-guestList.forEach(function (guestList) {
-    console.log("weelcome to dinner ".concat(guestList));
-});
+//Initial guest list YAQOOB909
+var guestList = ["Ali", "minal", "sam", "daniyal"];
+// for(let i = 0; i<guestList.length; i++){
+//   console.log('Dear Mr.' + '' + guestList [i] + '' + ', \n\n would you like to join me for dinner');
+// }
+var absent_Guest = 'minal';
+var new_Guest = 'ali';
+guestList[0] = new_Guest;
+for (var i = 0; i < guestList.length; i++) {
+    console.log('Dear Mr.' + '' + guestList[i] + '' + '\n\n   would you like to join me for dinner');
+    console.log("Dear Mr. ".concat(absent_Guest, " is not coming to the party."));
+}
--- app.ts
+++ app.ts
@@ -1,6 +1,28 @@
- let guestList = ['minal','zaid','saad'];
-  guestList.forEach(guestList => {
-  console.log(`weelcome to dinner party! ${guestList}`)
-  } );
+//Initial guest list YAQOOB909
+let guestList: string[] = ["Ali", "minal", "sam", "daniyal"];
+
+// for(let i = 0; i<guestList.length; i++){
+
+//   console.log('Dear Mr.' + '' + guestList [i] + '' + ', \n\n would you like to join me for dinner');
+// }
+
+let absent_Guest: string = 'minal';
+
+let new_Guest: string = 'ali';
+
+guestList[0] = new_Guest;
+
+for(let i = 0; i<guestList.length; i++){
+
+    console.log('Dear Mr.' + ''+  guestList [i] + '' + '\n\n   would you like to join me for dinner');
+    console.log(`Dear Mr. ${absent_Guest} is not coming to the party.`);
+
+}   
+  // console.log(`Dear Mr. ${absent_Guest} is not coming to the party.`);
+
+
+
+
+
 
 

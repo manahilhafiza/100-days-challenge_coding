0ea375b827da464c57617d79a6ae19f6380d4d21
Manahil <161109728+manahilhafiza@users.noreply.github.com>
2024-05-21 21:50:45 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-var friends = [' \n minal', 'sadia', 'yasir', 'zaid \n'];
-console.log("we are inviting for dinner ".concat(friends, " thanks!"));
+// Question 13: Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.
+var transports = [" motorcycle", "car", "cycle"];
+transports.forEach(function (transport) {
+    console.log("I would like to own a ".concat(transport, "."));
+});
--- app.ts
+++ app.ts
@@ -1,2 +1,7 @@
-let friends = [' \n minal','sadia','yasir','zaid \n'];
-console.log(`we are inviting for dinner ${friends} thanks!`);
+
+// Question 13: Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.
+let transports: string[] = [" motorcycle", "car", "cycle"];
+
+transports.forEach(transport => {
+    console.log(`I would like to own a ${transport}.`);
+});

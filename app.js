88ac39ab8bad6c95b1ad7d43d12d62b793c99336
Manahil <161109728+manahilhafiza@users.noreply.github.com>
2024-05-21 21:52:07 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// Question 13: Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.
-var transports = [" motorcycle", "car", "cycle"];
-transports.forEach(function (transport) {
-    console.log("I would like to own a ".concat(transport, "."));
-});
+var person = ["manal", "nida", "fiza"];
+var message = ['wellcome to typescript class.'];
+for (var i = 0; i < person.length; i++) {
+    console.log(message + person[i]);
+}

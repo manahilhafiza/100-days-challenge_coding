ca7c1b0c921b72fa0113385dd559b55406e9655c
Manahil <161109728+manahilhafiza@users.noreply.github.com>
2024-05-23 16:08:51 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -8,3 +8,13 @@ guestList.forEach(function (guestList) {
     console.log("Dear ".concat(guestList, ", \n\n would you like to join me for dinner?"));
     console.log("Dear Mr. ".concat(absent_Guest, " \n\n is not coming to the party."));
 });
+console.log("Unfortunately, \n\n I can only invite two people for dinner.");
+while (guestList.length > 2) {
+    var removedGuest = guestList.pop();
+    console.log("Sorry, ".concat(removedGuest, ",\n\n I can't invite you to dinner."));
+}
+guestList.forEach(function (guest) {
+    console.log("Dear ".concat(guest, ", \n\n you're still invited to dinner."));
+});
+guestList.splice(0, guestList.length);
+console.log(guestList);

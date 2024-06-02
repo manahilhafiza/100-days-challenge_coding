d5d5ab990bd61acb02eb7c2e396b05b1e9913b91
Manahil <161109728+manahilhafiza@users.noreply.github.com>
2024-06-02 15:31:45 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
-//  let guest: string [] = ["maria","ayesha","zaid","kamran","imran"];
-//  console.log(` iam  inviting  ${guest.length} to dinner! `);
-var biryani = "mutton";
-var raita = 'chhli ka';
-if (biryani == 'mutton') {
-    if (raita == 'chili ka') {
-        console.log('here is your biryani ');
-    }
-    else
-        ('or raita');
-}
-else {
-    console.log(' khAtam');
-}
-console.log(" ".concat(biryani, ", ").concat(raita));
+var fvt_country = ["canada", "new zealand", "switzerland"];
+console.log(" \n\n I'd like to visit countries \n\n ".concat(fvt_country, " "));
--- app.ts
+++ app.ts
@@ -1,18 +1,4 @@
-//  let guest: string [] = ["maria","ayesha","zaid","kamran","imran"];
-//  console.log(` iam  inviting  ${guest.length} to dinner! `);
-
-let biryani = "mutton";
-let raita = 'chhli ka';
-
-if(biryani == 'mutton'){
-  if(raita == 'chili ka'){
-    console.log('here is your biryani ');
-
-  }else('or raita');
-}
-else{
-  console.log(' khAtam');
-}
-
-
-
+ let fvt_country: string [] = ["canada","new zealand","switzerland"];
+ 
+ console.log(` \n\n I'd like to visit countries \n\n ${fvt_country} `);
+ 

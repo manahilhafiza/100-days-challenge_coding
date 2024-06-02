fc3bd91000e961a9315a7b6dda25f08842abce49
Manahil <161109728+manahilhafiza@users.noreply.github.com>
2024-06-02 15:28:43 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-var fvt_country = ["canada", "new zealand", "switzerland"];
-console.log(" \n\n I'd like to visit countries \n\n ".concat(fvt_country, " "));
+var guestList = ["Ali", "minal", "sam", "daniyal"];
+console.log("Great news! I found a bigger dinner table!");
+var absent_Guest = "minal";
+guestList.push('yasir');
+guestList.unshift('talha');
+guestList.splice(guestList.length / 2, 0, 'kamran');
+guestList.forEach(function (guestList) {
+    console.log("Dear ".concat(guestList, ", \n\n would you like to join me for dinner?"));
+    console.log("Dear Mr. ".concat(absent_Guest, " \n\n is not coming to the party."));
+});
--- app.ts
+++ app.ts
@@ -1,4 +1,15 @@
- let fvt_country: string [] = ["canada","new zealand","switzerland"];
- 
- console.log(` \n\n I'd like to visit countries \n\n ${fvt_country} `);
- 
+
+
+var guestList = ["Ali", "minal", "sam", "daniyal"];
+console.log("Great news! I found a bigger dinner table!");
+
+let absent_Guest= "minal";
+ guestList.push('yasir');
+ guestList.unshift('talha');
+ guestList.splice(guestList.length/ 2,0,'kamran');
+
+guestList.forEach(guestList => {
+ console.log(`Dear ${guestList}, \n\n would you like to join me for dinner?`);
+ console.log("Dear Mr. ".concat(absent_Guest, " \n\n is not coming to the party."));
+
+ });

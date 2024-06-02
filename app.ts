3a810b9a20ada3252a7f7582e1acf87e076565f8
Manahil <161109728+manahilhafiza@users.noreply.github.com>
2024-06-02 15:29:27 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -13,3 +13,16 @@ guestList.forEach(guestList => {
  console.log("Dear Mr. ".concat(absent_Guest, " \n\n is not coming to the party."));
 
  });
+
+console.log("Unfortunately, \n\n I can only invite two people for dinner.");
+ while (guestList.length > 2) {
+    let removedGuest = guestList.pop();
+    console.log(`Sorry, ${removedGuest},\n\n I can't invite you to dinner.`);
+}
+    
+guestList.forEach(guest => {
+    console.log(`Dear ${guest}, \n\n you're still invited to dinner.`);
+});
+guestList.splice(0, guestList.length);
+console.log(guestList);   
+
